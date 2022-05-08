b526acecc3342f45d5919266a4c80c73c930163c
Leo <56608247+leoyulee@users.noreply.github.com>
2022-05-08 13:39:58 -0500

    Removed unused service

--- src/init.lua
+++ src/init.lua
@@ -13,8 +13,6 @@ local ClassService = ArcaneumGlobals:GetGlobal("ClassService")
 ClassService:CheckVersion("1.1.0")
 local Class = ClassService:GetClass("Class")
 Class:CheckVersion("1.2.0")
-local Utilities = ArcaneumGlobals:GetGlobal("Utilities")
-Utilities:CheckVersion("1.0.0")
 local TestCollectionClass = require(script.TestCollection)
 --[=[
     @server
